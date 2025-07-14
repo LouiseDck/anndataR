faa0450f9597d7a8e8c7c98467278e39d9991e58
LouiseDck <louise.deconinck@gmail.com>
2025-07-14 16:16:22 +0200

    more debug prints

--- R/HDF5AnnData.R
+++ R/HDF5AnnData.R
@@ -560,7 +560,7 @@ HDF5AnnData <- R6::R6Class(
           gc()
         }
       } else {
-        print("Closing HDF5 file")
+        print(paste0("Closing HDF5 file: ", private$.h5obj$file))
         print(rhdf5::H5Iis_valid(private$.h5obj))
         if (rhdf5::H5Iis_valid(private$.h5obj)) {
           # tryCatch({
